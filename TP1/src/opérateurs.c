0d1f412bc5ba69a7d92b747e6acbda7f733197d2
ufzrihfuizr <144798872+ufzrihfuizr@users.noreply.github.com>
2025-11-26 00:59:57 +0100

    Update opérateurs.c

---
+++ TP1/src/opérateurs.c
@@ -0,0 +1,19 @@
+int main(int argc, char *argv[])
+{
+
+  int a= 16, b = 3; 
+  
+
+  
+  printf(" a+b = %d",a+b);
+    printf(" b-a = %d",b-a);
+    printf(" a*b = %d",a*b);
+    printf(" a/b = %d",a/b);
+     printf(" a%b = %d",a%b);
+    printf(" a=b is %bool",a=b);
+    printf(" a>b is %bool",a>b);
+    
+
+
+  return 0;
+}
